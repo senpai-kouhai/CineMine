a05eabe62dcab92f2e4ff2d52c7c3e0091d90f2a
senpai-kouhai <129678369+senpai-kouhai@users.noreply.github.com>
2023-06-19 12:28:14 +0000

    created review_function

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,11 +3,12 @@ class ReviewsController < ApplicationController
   def create
     @movie = Movie.find(params[:movie_id])
     @review = @movie.reviews.build(review_params)
-    @review.user = current_user
+    @review.user = current_user  # ここで、現在のユーザーをレビューの作成者として設定します。
     if @review.save
       redirect_to @movie, notice: 'レビューを投稿しました。'
     else
       flash.now[:error] = 'レビューの投稿に失敗しました。'
+      Rails.logger.info @review.errors.full_messages  # レビューのエラーメッセージをログに出力
       render 'movies/show'
     end
   end
