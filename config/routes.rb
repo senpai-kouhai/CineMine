32afbd35e23427e56b36e5d69ba0a12ae8a574e5
senpai-kouhai <129678369+senpai-kouhai@users.noreply.github.com>
2023-07-15 09:00:31 +0000

    sort_out_code

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,30 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
+
   root 'homes#top'
 
+  get 'homes/top'
+  get 'homes/about'
+
   resources :movies, only: [:index, :show] do
-    resources :reviews, only: [:create, :show, :edit, :destroy, :update] do
-      resources :likes, only: [:create, :destroy], module: :reviews
-      resources :comments, only: [:create, :destroy, :edit]
-    end
     member do
       post 'add_to_movielist'
       delete 'remove_from_movielist'
     end
+
     collection do
       get 'search'
+    end
+
+    resources :reviews, only: [:create, :show, :edit, :destroy, :update] do
+      resources :likes, only: [:create, :destroy], module: :reviews
+      resources :comments, only: [:create, :destroy, :edit]
     end
   end
 
   resources :reviews, only: [:index]
 
-  resources :users, only: [:show, :edit,:update, :destroy] do
+  resources :users, only: [:show, :edit, :update, :destroy] do
     collection do
       get 'userhome'
       get 'movielist'
@@ -26,9 +32,6 @@ Rails.application.routes.draw do
       get 'search'
     end
   end
-
-  get 'homes/top'
-  get 'homes/about'
 
   resources :relationships, only: [:create, :destroy]
 end
